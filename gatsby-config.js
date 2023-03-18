f9dbde19629ce9966210599791541e498b1a0447
martingreen-pe <martingreen.pe@gmail.com>
2023-03-18 17:33:31 -0500

    configurando filesistem3

--- gatsby-config.js
+++ gatsby-config.js
@@ -40,8 +40,8 @@ module.exports = {
     {
       resolve: `gatsby-source-filesystem`,
       options: {
-        name: `blog`,
-        path: `${__dirname}/content/blog`,
+        name: `content`,
+        path: `${__dirname}/content/`,
       },
     },
     {
